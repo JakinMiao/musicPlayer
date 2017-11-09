dc831de38123a9e35538b92486d320d78da5a69b
jakinYLiu <iOSLiuYong2012@kalengo.com>
2017-11-09 18:31:00 +0800

    初始化项目

---
+++ musicPlayer/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  musicPlayer
+//
+//  Created by Kalengo_2 on 2017/11/9.
+//  Copyright © 2017年 kalengo_2. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
